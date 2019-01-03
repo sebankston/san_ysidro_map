f57b0c7d32195d6b797ec449d22aa9cd668d90a4
Sam Bankston <sebankston@gmail.com>
2019-01-02 16:53:38 -0800

    nfm map

--- nfm_map.R
+++ nfm_map.R
@@ -16,7 +16,7 @@ gs_ws_ls(nfm_all)
 
 nfm_data <- gs_read(ss = nfm_all, ws = "Sheet1") %>% clean_names()
 
-# nfm_nr <- nfm_data %>% filter(barrier_type == "NR") 
+nfm_nr <- nfm_data %>% filter(barrier_type == "NR") 
 nfm_r <- nfm_data %>% filter(barrier_type == "R") 
 nfm_r2 <- nfm_data %>% filter(barrier_type == "R" & x2nd_pass == "Y") 
 
